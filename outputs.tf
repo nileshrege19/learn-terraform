da2dc3ca662b167890c3c6b16c3fdb86a5e88812
nilesh <nileshrege19@gmail.com>
2024-01-17 22:01:32 +0530

    Created shell script for Frontend

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "v1" {
   value = var.v1
 }
+
+output "First Value" {
+  value = var.v2[0]
+}
+
+output "Second Value" {
+  value = var.v1[1]
+}
+
+output "Value_in_Map" {
+  value = var.v3["abc"]
+}
--- var.tf
+++ var.tf
@@ -1,3 +1,14 @@
 variable "v1" {
   default = "Hello World"
 }
+
+variable "v2" {
+  default = ["Hello World", 100, true]
+}
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "Five Hundred"
+  }
+}
